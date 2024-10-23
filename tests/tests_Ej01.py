a2cb9518a784c763a47698f12016112e35af1118
Oscar-code21 <oarriazag@iesrafaelalberti.es>
2024-10-23 18:06:15 +0200

    Add files via upload

---
+++ tests/tests_Ej01.py
@@ -0,0 +1,14 @@
+from src.Ej01 import cuantas_veces
+def cuantas_veces():
+    assert cuantas_veces("b") == (
+        "1. b" +
+        "2. b" +
+        "3. b" +
+        "4. b" +
+        "5. b" +
+        "6. b" +
+        "7. b" +
+        "8. b" +
+        "9. b" +
+        "10. b"
+    )
