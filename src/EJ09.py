1b01f45c6668fc87d99a49db9160c72815c8281a
Oscar-code21 <oarriazag@iesrafaelalberti.es>
2024-10-23 12:42:50 +0200

    Add files via upload

---
+++ src/EJ09.py
@@ -0,0 +1,37 @@
+#Escribir un programa que almacene la cadena de caracteres contraseña en una variable, pregunte al usuario por la contraseña hasta que introduzca la contraseña correcta.
+
+def introduce_contraseña():
+    contraseña = input("Introduce la contraseña")
+    return contraseña
+
+
+
+
+def cadena_caracteres():
+    '''
+    Escribo desarrollo quue almacena la cadena de caracteres contraseña en una variable
+    '''
+    clave = "pablo"
+    Contraseña = ""
+    while Contraseña != clave:
+        Contraseña = introduce_contraseña() # pide la contraseña al usuario
+    return clave
+
+def contraseña_correcta(clave):
+    '''
+    aviso cuando la contraseña es correcta
+    '''
+    print(f"La contraseña '{clave}' es correcta.")
+
+
+def main ():
+    
+   #Desarrollo
+    clave = cadena_caracteres()
+
+    #Salida
+
+    contraseña_correcta (clave)
+
+if __name__=="__main__":
+    main() 
